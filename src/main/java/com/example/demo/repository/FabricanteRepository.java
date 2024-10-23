630b675912a2854327e45d497cc5f3666611ea4a
César Willian Pacheco <107158551+magocesar@users.noreply.github.com>
2024-10-23 18:33:00 -0300

    apaguei tudo

--- src/main/java/com/example/demo/repository/FabricanteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Fabricante;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FabricanteRepository extends JpaRepository<Fabricante, Integer> {
-    Fabricante findByCnpj(String cnpj);
-}
--- src/main/java/com/example/demo/repository/LoteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Lote;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LoteRepository extends JpaRepository<Lote, Integer> {
-}
